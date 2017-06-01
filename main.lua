8224aaa0ed40ea43d670f38c027e7446b663a29f
Ajusa Jain <arhamjain@gmail.com>
2017-05-31 20:50:28 -0400

    game now has asteroids

--- main.lua
+++ main.lua
@@ -6,18 +6,9 @@ local playerProj
 playerProj = function(p, o, w2, h2)
   return (p.x - p.w / 2) < o.x + w2 and o.x < (p.x - p.w / 2) + p.w and (p.y - p.h / 2) < o.y + h2 and o.y < (p.y - p.h / 2) + p.h
 end
-local createStars
-createStars = function()
-  if stars == nil then
-    stars = { }
-    for i = 1, 40 do
-      stars[i] = {
-        Size = love.math.random(1, 8),
-        XPosition = love.math.random(1, 1200),
-        YPosition = love.math.random(1, 750)
-      }
-    end
-  end
+local asteroidProj
+asteroidProj = function(x1, y1, w1, h1, x2, y2, w2, h2)
+  return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + h1
 end
 love.graphics.setDefaultFilter("nearest")
 love.window.setFullscreen(true)
@@ -48,12 +39,98 @@ do
   _base_0.__class = _class_0
   Entity = _class_0
 end
+local Asteroid
+do
+  local _class_0
+  local _parent_0 = Entity
+  local _base_0 = {
+    draw = function(self)
+      return love.graphics.draw(self.p.image, self.p.x, self.p.y, 0, self.p.scale, self.p.scale)
+    end,
+    update = function(self, dt)
+      for i = #bullets, 1, -1 do
+        if asteroidProj(self.p.x, self.p.y, self.p.scale * 16, self.p.scale * 16, bullets[i].p.x, bullets[i].p.y, 6, 6) then
+          self.p.lives = self.p.lives - 1
+          self.p.dx = self.p.dx + (bullets[i].p.dx / 15)
+          self.p.dy = self.p.dy + (bullets[i].p.dy / 15)
+          table.remove(bullets, i)
+        end
+      end
+      if isInBounds({
+        x = self.p.x + self.p.dx * dt,
+        y = self.p.y + self.p.dy * dt
+      }) then
+        return _class_0.__parent.__base.update(self, dt)
+      else
+        self.p.dx, self.p.dy = self.p.dx / -1.1, self.p.dy / -1.1
+      end
+    end
+  }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _class_0.__parent.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "Asteroid",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  Asteroid = _class_0
+end
 local Player
 do
   local _class_0
   local _parent_0 = Entity
   local _base_0 = {
+    checkForBullets = function(self, dt)
+      for i = #bullets, 1, -1 do
+        if bullets[i].p.from ~= self.p.type and playerProj(self.p, bullets[i].p, 6, 6) then
+          self.p.lives = self.p.lives - love.math.random(5, 12)
+          table.remove(bullets, i)
+        end
+      end
+    end,
+    checkForAsteroids = function(self, dt)
+      for i = #asteroids, 1, -1 do
+        if playerProj(self.p, asteroids[i].p, asteroids[i].p.scale * 16, asteroids[i].p.scale * 16) and (love.timer.getTime() - start) > 3 then
+          self.p.lives = self.p.lives - (love.math.random(2, 7) * asteroids[i].p.scale)
+          table.remove(asteroids, i)
+        end
+      end
+    end,
+    draw = function(self)
+      love.graphics.print(self.p.type .. " Health: " .. self.p.lives .. "%", 50 + self.p.offset, 20)
+      love.graphics.draw(self.p.image, self.p.x, self.p.y, self.p.angle, 2, 2, 8, 8)
+      if self.p.lives < 1 then
+        love.graphics.print(self.p.type .. " Loses! ", 300, 200)
+        return love.graphics.draw(bomb, self.p.x, self.p.y, self.p.angle, 2, 2, 8, 8)
+      end
+    end,
     update = function(self, dt)
+      self:checkForBullets(dt)
+      self:checkForAsteroids(dt)
       self.p.timeFire = self.p.timeFire + dt
       if love.keyboard.isDown(self.p.left) then
         self.p.angle = self.p.angle - (self.p.turnSpeed * dt)
@@ -70,14 +147,13 @@ do
       if love.keyboard.isDown(self.p.fire) then
         if self.p.timeFire > self.p.fireDelay then
           self.p.timeFire = 0
-          local bullet = Entity({
+          table.insert(bullets, Entity({
             x = self.p.x,
             y = self.p.y,
             dx = self.p.dx + self.p.bulletSpeed * math.sin(self.p.angle),
             dy = self.p.dy + (-self.p.bulletSpeed * math.cos(self.p.angle)),
             from = self.p.type
-          })
-          table.insert(bullets, bullet)
+          }))
         end
       end
       if isInBounds({
@@ -86,10 +162,9 @@ do
       }) then
         _class_0.__parent.__base.update(self, dt)
       else
-        self.p.dx = self.p.dx / -2
-        self.p.dy = self.p.dy / -2
+        self.p.dx, self.p.dy = self.p.dx / -2, self.p.dy / -2
       end
-      self.p.dy, self.p.dx = self.p.dy * .99, self.p.dx * .99
+      self.p.dy, self.p.dx = self.p.dy * .99, self.p.dx * .995
     end
   }
   _base_0.__index = _base_0
@@ -125,84 +200,113 @@ do
   end
   Player = _class_0
 end
-local tie = Player({
-  x = 300,
-  y = 300,
-  w = 16,
-  h = 16,
-  dx = 0,
-  dy = 0,
-  speed = 1,
-  angle = 0,
-  turnSpeed = 1.2,
-  fireDelay = .5,
-  timeFire = 0,
-  bulletSpeed = 200,
-  lives = 5,
-  left = "a",
-  right = "d",
-  forward = "w",
-  backward = "s",
-  fire = "space",
-  type = "tie"
-})
-local xwing = Player({
-  x = 700,
-  y = 700,
-  w = 16,
-  h = 16,
-  dx = 0,
-  dy = 0,
-  speed = .75,
-  angle = 0,
-  turnSpeed = 1.2,
-  fireDelay = .4,
-  timeFire = 0,
-  bulletSpeed = 200,
-  lives = 5,
-  left = "left",
-  right = "right",
-  forward = "up",
-  backward = "down",
-  fire = "rctrl",
-  type = "xwing"
-})
-local game = true
-bullets = { }
 love.load = function()
-  local font = love.graphics.newFont("kenpixel.ttf", 14)
-  love.graphics.setFont(font)
+  start = love.timer.getTime()
+  tie = Player({
+    x = 300,
+    y = 300,
+    w = 16,
+    h = 16,
+    dx = 0,
+    dy = 0,
+    speed = 1,
+    angle = 0,
+    turnSpeed = 1.2,
+    fireDelay = .5,
+    timeFire = 0,
+    bulletSpeed = 200,
+    lives = 100,
+    left = "a",
+    right = "d",
+    forward = "w",
+    backward = "s",
+    fire = "space",
+    type = "Tie Fighter",
+    offset = 0
+  })
+  xwing = Player({
+    x = 700,
+    y = 700,
+    w = 16,
+    h = 16,
+    dx = 0,
+    dy = 0,
+    speed = .75,
+    angle = 0,
+    turnSpeed = 1.2,
+    fireDelay = .4,
+    timeFire = 0,
+    bulletSpeed = 200,
+    lives = 100,
+    left = "left",
+    right = "right",
+    forward = "up",
+    backward = "down",
+    fire = "rctrl",
+    type = "X-Wing",
+    offset = 250
+  })
+  bullets = { }
+  love.graphics.setFont(love.graphics.newFont("kenpixel.ttf", 14))
   tie.p.image = love.graphics.newImage("tie.png")
   xwing.p.image = love.graphics.newImage("xwing.png")
+  stars = { }
+  for i = 1, 40 do
+    stars[i] = {
+      size = love.math.random(1, 8),
+      x = love.math.random(1, love.graphics.getWidth()),
+      y = love.math.random(1, love.graphics.getHeight())
+    }
+  end
+  asteroids = { }
+  for i = 1, love.math.random(4, 10) do
+    asteroids[i] = Asteroid({
+      lives = 5,
+      x = love.math.random(1, 1200),
+      y = love.math.random(1, 750),
+      dx = love.math.random(-40, 40),
+      dy = love.math.random(-40, 40),
+      image = love.graphics.newImage("asteroid" .. love.math.random(1, 3) .. ".png"),
+      scale = love.math.random(1.5, 4)
+    })
+  end
   bomb = love.graphics.newImage("bomb.png")
   death = love.graphics.newImage("death.png")
 end
 love.update = function(dt)
-  if game then
-    for i = #bullets, 1, -1 do
-      bullets[i]:update(dt)
-      if bullets[i].p.from == "xwing" and (playerProj(tie.p, bullets[i].p, 6, 6)) then
-        tie.p.lives = tie.p.lives - 1
-        table.remove(bullets, i)
-      elseif bullets[i].p.from == "tie" and (playerProj(xwing.p, bullets[i].p, 6, 6)) then
-        xwing.p.lives = xwing.p.lives - 1
-        table.remove(bullets, i)
+  if xwing.p.lives > 0 and tie.p.lives > 0 then
+    local _list_0 = bullets
+    for _index_0 = 1, #_list_0 do
+      local bullet = _list_0[_index_0]
+      bullet:update(dt)
+    end
+    local _list_1 = asteroids
+    for _index_0 = 1, #_list_1 do
+      local asteroid = _list_1[_index_0]
+      asteroid:update(dt)
+    end
+    for i = #asteroids, 1, -1 do
+      if asteroids[i].p.lives < 0 then
+        table.remove(asteroids, i)
       end
     end
     tie:update(dt)
     return xwing:update(dt)
+  else
+    if love.keyboard.isDown("return") then
+      return love.load()
+    end
   end
 end
 love.draw = function()
-  createStars()
-  for _, star in ipairs(stars) do
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.rectangle("fill", star.XPosition, star.YPosition, star.Size, star.Size)
+  local _list_0 = stars
+  for _index_0 = 1, #_list_0 do
+    local star = _list_0[_index_0]
+    love.graphics.rectangle("fill", star.x, star.y, star.size, star.size)
   end
-  love.graphics.setColor(255, 255, 255)
   love.graphics.draw(death, 600, 300)
   for i, v in ipairs(bullets) do
-    if v.p.from == "xwing" then
+    if v.p.from == "X-Wing" then
       love.graphics.setColor(255, 0, 0)
     else
       love.graphics.setColor(0, 255, 0)
@@ -210,18 +314,11 @@ love.draw = function()
     love.graphics.circle("fill", v.p.x, v.p.y, 3)
   end
   love.graphics.setColor(255, 255, 255)
-  love.graphics.print("Tie Lives: " .. tie.p.lives, 50, 20)
-  love.graphics.print("X-Wing Lives: " .. xwing.p.lives, 200, 20)
-  love.graphics.draw(tie.p.image, tie.p.x, tie.p.y, tie.p.angle, 2, 2, 8, 8)
-  love.graphics.draw(xwing.p.image, xwing.p.x, xwing.p.y, xwing.p.angle, 2, 2, 8, 8)
-  if xwing.p.lives < 1 then
-    love.graphics.print("Tie Fighter Wins! ", 300, 200)
-    love.graphics.draw(bomb, xwing.p.x, xwing.p.y, xwing.p.angle, 2, 2, 8, 8)
-    game = false
-  end
-  if tie.p.lives < 1 then
-    love.graphics.print("X-Wing Wins! ", 300, 200)
-    love.graphics.draw(bomb, tie.p.x, tie.p.y, tie.p.angle, 2, 2, 8, 8)
-    game = false
+  tie:draw()
+  xwing:draw()
+  local _list_1 = asteroids
+  for _index_0 = 1, #_list_1 do
+    local asteroid = _list_1[_index_0]
+    asteroid:draw()
   end
 end
--- main.moon
+++ main.moon
@@ -1,19 +1,6 @@
-isInBounds = (p) ->  
-	p.x > 0 and p.y > 0 and p.x<love.graphics.getWidth() and p.y<love.graphics.getHeight()
-playerProj = (p,o,w2,h2) ->
-  return (p.x-p.w/2) < o.x+w2 and
-         o.x < (p.x-p.w/2)+p.w and
-         (p.y-p.h/2) < o.y+h2 and
-         o.y < (p.y-p.h/2)+p.h
-createStars = ->
-    if stars==nil then
-        export stars={}
-        for i = 1, 40 do
-			stars[i] = {
-                Size: love.math.random(1, 8),
-                XPosition: love.math.random(1, 1200),
-                YPosition: love.math.random(1, 750),
-            }
+isInBounds = (p) -> p.x > 0 and p.y > 0 and p.x<love.graphics.getWidth() and p.y<love.graphics.getHeight()
+playerProj = (p,o,w2,h2) -> (p.x-p.w/2) < o.x+w2 and o.x < (p.x-p.w/2)+p.w and (p.y-p.h/2) < o.y+h2 and o.y < (p.y-p.h/2)+p.h
+asteroidProj = (x1,y1,w1,h1, x2,y2,w2,h2) -> x1 < x2+w2 and x2 < x1+w1 and y1 < y2+h2 and y2 < y1+h1
 love.graphics.setDefaultFilter("nearest")
 love.window.setFullscreen(true)
 class Entity
@@ -21,70 +8,80 @@ class Entity
   update: (dt) =>
 	@p.x += @p.dx*dt
 	@p.y += @p.dy*dt
-class Player extends Entity
+class Asteroid extends Entity
+	draw: => 
+		love.graphics.draw(@p.image, @p.x, @p.y, 0, @p.scale, @p.scale)
 	update: (dt) =>
+		for i=#bullets,1,-1 do
+    		if  asteroidProj(@p.x, @p.y, @p.scale*16, @p.scale*16, bullets[i].p.x, bullets[i].p.y, 6,6)
+    	  		@p.lives -= 1
+    	  		@p.dx += bullets[i].p.dx/15
+    	  		@p.dy += bullets[i].p.dy/15
+    	  		table.remove(bullets, i)
+		if isInBounds({x:@p.x + @p.dx*dt, y:@p.y + @p.dy*dt}) then super dt
+		else @p.dx, @p.dy = @p.dx/-1.1, @p.dy/-1.1
+class Player extends Entity
+	checkForBullets: (dt) =>
+		for i=#bullets,1,-1 do
+    		if bullets[i].p.from ~= @p.type and playerProj(@p, bullets[i].p, 6,6)
+    	  		@p.lives -= love.math.random(5, 12)
+    	  		table.remove(bullets, i)
+    checkForAsteroids: (dt) =>
+		for i=#asteroids,1,-1 do
+    		if playerProj(@p, asteroids[i].p, asteroids[i].p.scale*16, asteroids[i].p.scale*16) and (love.timer.getTime() - start) > 3 --invincible for 3 seconds
+    	  		@p.lives -= love.math.random(2, 7)*asteroids[i].p.scale
+    	  		table.remove(asteroids, i)
+	draw: => 
+		love.graphics.print(@p.type .. " Health: " .. @p.lives .. "%", 50 + @p.offset, 20)
+		love.graphics.draw(@p.image, @p.x, @p.y, @p.angle, 2, 2, 8, 8)
+		if @p.lives < 1
+   			love.graphics.print(@p.type .. " Loses! ", 300, 200)
+   			love.graphics.draw(bomb, @p.x, @p.y, @p.angle, 2, 2, 8, 8)
+	update: (dt) =>
+		@checkForBullets(dt)
+		@checkForAsteroids(dt)
 		@p.timeFire += dt
 		if love.keyboard.isDown @p.left then @p.angle -= @p.turnSpeed*dt
 		if love.keyboard.isDown @p.right then @p.angle += @p.turnSpeed*dt
-		if love.keyboard.isDown @p.forward
-			@p.dy, @p.dx = @p.dy - (@p.speed * math.cos(@p.angle)), @p.dx + (@p.speed * math.sin(@p.angle))
-		if love.keyboard.isDown @p.backward
-			@p.dy, @p.dx = @p.dy + (@p.speed * math.cos(@p.angle)), @p.dx - (@p.speed * math.sin(@p.angle))
+		if love.keyboard.isDown @p.forward then @p.dy, @p.dx = @p.dy - (@p.speed * math.cos(@p.angle)), @p.dx + (@p.speed * math.sin(@p.angle))
+		if love.keyboard.isDown @p.backward then @p.dy, @p.dx = @p.dy + (@p.speed * math.cos(@p.angle)), @p.dx - (@p.speed * math.sin(@p.angle))
 		if love.keyboard.isDown @p.fire
 			if @p.timeFire > @p.fireDelay then
         		@p.timeFire = 0
-        		bullet = Entity {x: @p.x, y: @p.y, dx: @p.dx+ @p.bulletSpeed * math.sin(@p.angle), dy: @p.dy + (-@p.bulletSpeed * math.cos(@p.angle)), from: @p.type}
-        		table.insert(bullets, bullet)
+        		table.insert(bullets, Entity {x: @p.x, y: @p.y, dx: @p.dx+ @p.bulletSpeed * math.sin(@p.angle), dy: @p.dy + (-@p.bulletSpeed * math.cos(@p.angle)), from: @p.type})
 		if isInBounds({x:@p.x + @p.dx*dt, y:@p.y + @p.dy*dt}) then super dt
-		else
-			@p.dx /= -2
-			@p.dy /= -2
-		@p.dy, @p.dx = @p.dy*.99, @p.dx*.99
-tie = Player x: 300, y: 300, w: 16, h: 16, dx: 0, dy: 0, speed: 1, angle: 0, turnSpeed: 1.2, fireDelay: .5, timeFire: 0, bulletSpeed: 200, lives: 5, left:"a", right:"d", forward:"w", backward:"s", fire:"space", type:"tie"
-xwing = Player x: 700, y: 700, w: 16, h: 16, dx: 0, dy: 0, speed: .75, angle: 0, turnSpeed: 1.2, fireDelay: .4, timeFire: 0, bulletSpeed: 200, lives: 5, left:"left", right:"right", forward:"up", backward:"down", fire: "rctrl", type: "xwing"
-game = true
-export bullets = {}
+		else @p.dx, @p.dy = @p.dx/-2, @p.dy/-2
+		@p.dy, @p.dx = @p.dy*.99, @p.dx*.995 --friction
 love.load = ->
-    font = love.graphics.newFont("kenpixel.ttf", 14)
-    love.graphics.setFont(font)
+	export start = love.timer.getTime()
+	export tie = Player x: 300, y: 300, w: 16, h: 16, dx: 0, dy: 0, speed: 1, angle: 0, turnSpeed: 1.2, fireDelay: .5, timeFire: 0, bulletSpeed: 200, lives: 100, left:"a", right:"d", forward:"w", backward:"s", fire:"space", type:"Tie Fighter", offset: 0
+	export xwing = Player x: 700, y: 700, w: 16, h: 16, dx: 0, dy: 0, speed: .75, angle: 0, turnSpeed: 1.2, fireDelay: .4, timeFire: 0, bulletSpeed: 200, lives: 100, left:"left", right:"right", forward:"up", backward:"down", fire: "rctrl", type: "X-Wing", offset: 250
+	export bullets = {}
+    love.graphics.setFont(love.graphics.newFont("kenpixel.ttf", 14))
     tie.p.image = love.graphics.newImage("tie.png")
     xwing.p.image = love.graphics.newImage("xwing.png")
+    export stars={}
+    for i = 1, 40 do stars[i] = size: love.math.random(1, 8), x: love.math.random(1, love.graphics.getWidth()), y: love.math.random(1, love.graphics.getHeight())
+    export asteroids = {}
+    for i = 1, love.math.random(4, 10) do asteroids[i] = Asteroid lives: 5, x: love.math.random(1, 1200), y: love.math.random(1, 750), dx: love.math.random(-40, 40), dy: love.math.random(-40, 40), image: love.graphics.newImage("asteroid"..love.math.random(1, 3)..".png"), scale: love.math.random(1.5, 4)
     export bomb = love.graphics.newImage("bomb.png")
     export death = love.graphics.newImage("death.png")
 love.update = (dt) ->
-	if game
-		for i=#bullets,1,-1 do
-			bullets[i]\update(dt)
-    		if bullets[i].p.from == "xwing" and (playerProj(tie.p, bullets[i].p, 6,6)) then
-    	  		tie.p.lives = tie.p.lives - 1
-    	  		table.remove(bullets, i)
-			elseif bullets[i].p.from == "tie" and (playerProj(xwing.p, bullets[i].p, 6,6)) then
-    	  		xwing.p.lives = xwing.p.lives - 1
-    	  		table.remove(bullets, i)
+	if xwing.p.lives > 0 and tie.p.lives > 0
+		for bullet in *bullets do bullet\update(dt)
+		for asteroid in *asteroids do asteroid\update(dt)
+		for i=#asteroids,1,-1 do if asteroids[i].p.lives < 0 then table.remove(asteroids, i)
 		tie\update(dt)
 		xwing\update(dt)
+	else if love.keyboard.isDown "return" then love.load!
 love.draw = ->
-	createStars()
-	for _, star in ipairs(stars) do
-        love.graphics.setColor(255, 255, 255)
-        love.graphics.rectangle("fill", star.XPosition, star.YPosition, star.Size, star.Size)
-	love.graphics.setColor(255,255,255)
-	love.graphics.draw(death, 600, 300)
+	for star in *stars do love.graphics.rectangle("fill", star.x, star.y, star.size, star.size)   
+	love.graphics.draw(death, 600, 300) --deathstar
     for i,v in ipairs(bullets) do
-    	if v.p.from == "xwing"
-			love.graphics.setColor(255, 0, 0)
+    	if v.p.from == "X-Wing" then love.graphics.setColor(255, 0, 0)
 		else love.graphics.setColor(0, 255, 0)
 		love.graphics.circle("fill", v.p.x, v.p.y, 3)
 	love.graphics.setColor(255,255,255)
-	love.graphics.print("Tie Lives: " .. tie.p.lives, 50, 20)
-	love.graphics.print("X-Wing Lives: " .. xwing.p.lives, 200, 20)
-	love.graphics.draw(tie.p.image, tie.p.x, tie.p.y, tie.p.angle, 2, 2, 8, 8)
-	love.graphics.draw(xwing.p.image, xwing.p.x, xwing.p.y, xwing.p.angle, 2, 2, 8, 8)
-	if xwing.p.lives < 1
-   		love.graphics.print("Tie Fighter Wins! ", 300, 200)
-   		love.graphics.draw(bomb, xwing.p.x, xwing.p.y, xwing.p.angle, 2, 2, 8, 8)
-   		game = false
-	if tie.p.lives < 1
-    	love.graphics.print("X-Wing Wins! ", 300, 200)
-    	love.graphics.draw(bomb, tie.p.x, tie.p.y, tie.p.angle, 2, 2, 8, 8)
-    	game = false
+	tie\draw!
+	xwing\draw!
+	for asteroid in *asteroids do asteroid\draw!
